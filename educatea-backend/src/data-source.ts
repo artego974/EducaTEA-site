9c69f8e060c59c3b58afb704d0280421d312e10a
artego974 <artego974@gmail.com>
2026-05-25 15:38:11 -0300

    retirar o syncronize

--- educatea-backend/src/data-source.ts
+++ educatea-backend/src/data-source.ts
@@ -18,7 +18,6 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASS || "root",
   database: process.env.DB_NAME || "educatea",
   timezone: "Z",
-  synchronize: process.env.NODE_ENV !== "production",
   logging: process.env.NODE_ENV === "development",
   entities: [User, Comment, Like, ChatbotSession, ChatbotMessage, ChatbotFeedback, NewsArticle, AccessibilitySettings, SavedPost],
   migrations: [],
